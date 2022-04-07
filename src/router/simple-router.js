914235245d2a9ffb5aa345698e32d35ddaf1f255
niltonc <niltoncesarjr1@gmail.com>
2022-04-07 15:39:53 -0300

    refactor(router): cleaning up

--- src/router/simple-router.js
+++ src/router/simple-router.js
@@ -2,11 +2,7 @@ import React from 'react'
 import {Switch,Route} from 'react-router-dom'
 
 // auth
-import ConfirmMail from '../views/dashboard/auth/confirm-mail'
-import LockScreen from '../views/dashboard/auth/lock-screen'
-import Recoverpw from '../views/dashboard/auth/recoverpw'
 import SignIn from '../views/dashboard/auth/sign-in'
-import SignUp from '../views/dashboard/auth/sign-up'
 // errors
 import Error404 from '../views/dashboard/errors/error404'
 import Error500 from '../views/dashboard/errors/error500'
@@ -17,11 +13,7 @@ const SimpleRouter = () => {
             <>
             <Switch>
                 {/* auth */}
-                <Route exact path="/auth/confirm-mail" component={ConfirmMail}/>
-                <Route exact path="/auth/lock-screen"  component={LockScreen}/>
-                <Route exact path="/auth/recoverpw"    component={Recoverpw}/>
                 <Route exact path="/auth/sign-in"      component={SignIn}/>
-                <Route exact path="/auth/sign-up"      component={SignUp}/>  
                 {/* error */}
                 <Route exact path="/errors/error404"   component={Error404}/>  
                 <Route exact path="/errors/error500"  component={Error500}/>
